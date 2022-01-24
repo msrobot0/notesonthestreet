2d78714bffe6c78bad91d9f96affef1c632e3098
meredith <meredith@paom.com>
2022-01-24 01:56:57 -0500

    mostly works for epub

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ::Notes On the Street
 
-This turns a list of pdfs into images with the name of the pdf written at the top of the image
+This turns a list of pdfs,epubs and mobis into images with the name of the pdf written at the top of the image
 
 It is alpha
 
@@ -8,6 +8,7 @@ i have only tested in on osx 10.15
 
 To run  on osx 
 brew install poppler (https://formulae.brew.sh/formula/poppler)
+brew install pango libffi
 
 fire up the command line and type
 
@@ -15,7 +16,7 @@ You can use venv (I do ) and do this in venv
 
 
 python -r requirements.txt
-python main.py python main.py source_dir dest_dir optional_max_pdfs
+python main.py source_dir dest_dir optional_max_pdfs
 
 I suppose I could make a nice install script and chown things - maybe in the future
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-   
+
+import lxml
 from bs4 import BeautifulSoup
 from weasyprint import HTML, CSS, default_url_fetcher
 from weasyprint.fonts import FontConfiguration
@@ -8,7 +9,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab.lib.colors import green
-import shutil
+import shutil,zipfile
 from io import BytesIO
 
 #from PyPDF2 import PdfFileWriter,PdfFileReader,PdfFileMerger
@@ -18,176 +19,222 @@ from reportlab.pdfbase.pdfmetrics import stringWidth
 
 import random,os,sys
 
-
 #mode is img data css css
-def get_epub_data(path,filename):
+class Epub:
+    def __init__(self,path):
+        self.path = path
+        self.new_path = None
+        self.new_filename= None
+        self.root = ""
 
-    f=open(path+"META-INF/"+"container.xml", "r")
-    data=f.read()
-    soup = BeautifulSoup(data, 'html.parser')
-    rootfile=soup.find_all("rootfile")
-    try:
-        rootfile = rootfile[0]
-        fullpath=rootfile.get("full-path")
-        if "/" in fullpath:
-            first,second = fullpath.split("/")[:2]      
-            
-            new_path= path+first+"/"
-            f=open(new_path+second,"r")
-            soup = BeautifulSoup(f.read(), 'lxml')
-            a=soup.find("manifest")
-            ncx_file=a.find(id="ncx").get("href")
-            f.close()
-            if(ncx_file is None):
-                print ("err 41")
-                return None
-                
-            f=open(os.path.join(new_path,ncx_file),"r")
-            return f.read()
-        else:
-            
-            f=open(os.path.join(path,fullpath),"r")
-            soup = BeautifulSoup(f.read(), 'lxml')
-            a=soup.find("manifest")
-            ncx_file=a.find(id="ncx").get("href")
-            f.close()
-            if(ncx_file is None):
-                print("NCX file not found 55")
-                
-            f.close()
-            f=open(os.path.join(path,ncx_file),"r")
-            return f.read()
-    except:
-        print ("err 61")
-        return None
+    #this is sort of crap code
+    def get_data(self):
+        self.get_epub_data()
+        im, css,data = self.get_decorations()
+        return im,css, data
 
-def get_epub(mode="img",path,filename):
-    f=open(path+filename,"r")
-    soup = BeautifulSoup(f.read(), 'lxml')
-    a=soup.find("manifest")
-    if mode == "img":
+    def get_epub_data(self):
+        f=open(self.path+"META-INF/"+"container.xml", "r")
+        data=f.read()
+        soup = BeautifulSoup(data, 'html.parser')
+        rootfile=soup.find_all("rootfile")
         try:
-            img_jpg=a.find("item",{"media-type":"image/jpeg"}).get("href")
-            return path+img_jpg.split("/")[0]+"/"
+
+            rootfile = rootfile[0]
+            fullpath=rootfile.get("full-path")
+
+            if "/" in fullpath:
+                first,second = fullpath.split("/")[:2]
+                self.new_path= self.path+first+"/"
+                self.new_filename = self.new_path+second
+                self.root = self.new_filename.split(".")[-1]
+            else:
+                self.new_path = fullpath
+                self.root = self.new_path.split(".")[-1]
+
+            self.root = self.root+":"
+        except Exception as er:
+
+            raise Exception("data path error")
+
+    def get_decorations(self):
+        f=open(self.new_filename,"r")
+        soup = BeautifulSoup(f.read(), 'lxml')
+
+        a=soup.find(self.root+"manifest")
+        if a is None:
+            self.root = ""
+            a=soup.find(self.root+"manifest")
+        img = None
+        data =None
+
+        try:
+            pages=a.find_all(self.root+"item",{"media-type":"application/xhtml+xml"})
+            data = pages[random.randint(0,len(pages)-1)].get("href")
+            f=open(os.path.join(self.new_path,data),"r")
+            data = f.read()
+            
+        except:
+            pass
+
+        try:
+            img_jpg=a.find(self.root+"item",{"media-type":"image/jpeg"}).get("href")
+            img = os.path.join(self.new_path,img_jpg.split("/")[0]+"/")
         except:
             pass
         try:
-            img_png=a.find("item",{"media-type":"image/png"}).get("href")
-            return global_root_dir+img_png.split("/")[0]+"/"
+            img_png=a.find(self.root+"item",{"media-type":"image/png"}).get("href")
+            img =  os.path.join(self.new_path,img_png.split("/")[0]+"/")
         except:
-            return None
+            pass
 
-
-    if mode == "css":
-        css_file=a.find("item",{"media-type":"text/css"}).get("href")
-        if css_file is None: 
-            return None
-        else: 
-            return [os.path.join(path,css_file)]
         
+        css_file = None
+        try:
+            css_file=a.find(self.root+"item",{"media-type":"text/css"}).get("href")
+            if css_file is not None:
+                css_file  = [os.path.join(self.new_path,css_file)]
+        except:
+            pass
+
+        return img, css_file, data
+
+
 
 def main():
     args = len(sys.argv)
     total =None
     path = ""
     new_path = ""
-    try: 
+    try:
         if sys.argv[1] == "d":
 
-            new_path = sys.argv[2]    
+            new_path = sys.argv[2]
             shutil.rmtree(new_path)
             os.makedirs(new_path)
-            return 
-        else:    
+            return
+        else:
             path = sys.argv[1]
             new_path = sys.argv[2]
             try:
               total = int(argv[3])
             except:
               pass
-    except: 
-    	print("Format is: python main.py source_dir dest_dir optional_max_pdfs")
+    except:
+        print("Format is: python main.py source_dir dest_dir optional_max_pdfs")
     files = os.listdir(path)
-    
+
     counter = 0
+    pdf_tmp = "/tmp/pdf_tmp/"
+    try:
+                shutil.rmtree(pdf_tmp)
+    except:
+        pass
+
+    os.mkdir(pdf_tmp)
+
     for fn in files:
-    	pdf_file = None
-    	in_pdf_file = os.path.join(path,fn)
-    	if fn[-4:] == "mobi":
-			tempdir, filepath = mobi.extract(in_pdf_file)
-			image_base=filepath[:-9]
-			html=HTML(filename=filepath,base_url=image_base,encoding="utf8")
-			pdf_filename=unzip_file_path.split("/")[-1]
-    		pdf_file = fn.replace(".mobi",".pdf")
-            html.write_pdf(new_filename)
-    		shutil.rmtree(tempdir)
-    	elif fn[-4:] == "epub":
-             
-              html=HTML(string=get_epub_data(path,fn),base_url=get_epub("img",path,fn),encoding="utf8")
-              pdf_file = fn.replace(".epub",".pdf")
-              html.write_pdf(pdf_file stylesheets=get_epub("css",path,fn),font_config=FontConfiguration())
-              print "ok"
-        if False #fn[-3:] == "pdf" or pdf_file is not None:
-          try:
-             counter += 1
-             if total is not None and  counter > total:
-               return
-             output = PdfFileWriter()
-             print(in_pdf_file)
-             pdf_file = PdfFileReader(open(in_pdf_file,"rb"))
-             num_pages = pdf_file.getNumPages()
-             
-             i = 0
-             if num_pages > 5:
+        pdf_file = None
+        in_pdf_file = os.path.join(path,fn)
+        if fn[-4:] == "mobi":
+            #tempdir, filepath = mobi.extract(in_pdf_file)
+            #image_base=filepath[:-9]
+            #html=HTML(filename=filepath,base_url=image_base,encoding="utf8")
+            #pdf_filename=unzip_file_path.split("/")[-1]
+            #pdf_file = fn.replace(".mobi",".pdf")
+            #in_pdf_file = pdf_tmp+pdf_file
+            #html.write_pdf(in_pdf_file)
+            #shutil.rmtree(tempdir)
+            pass
+
+        elif fn[-4:] == "epub":
+            try:
+                tmp_file = "/tmp/epub_temp.zip"
+                shutil.copy(in_pdf_file,tmp_file)
+                tmp_path="/tmp/epub_temp/"
+                try:
+                    shutil.rmtree(tmp_path)
+                except:
+                    pass
+
+                os.mkdir(tmp_path)
+                with zipfile.ZipFile(tmp_file, 'r') as zip_ref:
+                    ret=zip_ref.extractall(tmp_path)
+
+                pdf_file = fn.replace(".epub",".pdf")
+                in_pdf_file = pdf_tmp+pdf_file
+                epub = Epub(tmp_path)
+                img,css,data = epub.get_data()
+                html=HTML(string=data,base_url=img,encoding="utf8")
+                html.write_pdf(in_pdf_file,stylesheets=css,font_config=FontConfiguration())
+
+                shutil.rmtree(tmp_path)
+            except Exception as e:
+                print(e)
+                print("err %s" %fn)
+        if fn[-3:] == "pdf" or pdf_file is not None:
+            try:
+                counter += 1
+                if total is not None and  counter > total:
+                    return
+                output = PdfFileWriter()
+                print(in_pdf_file)
+                pdf_file = PdfFileReader(open(in_pdf_file,"rb"))
+                num_pages = pdf_file.getNumPages()
+
+                i = 0
+                if num_pages > 5:
                  i = 3
-             i = random.randrange(i,num_pages)
-             packet = BytesIO()
-             cv=canvas.Canvas(packet,pagesize=letter)
-             
-             
-             title = fn[:-3]+" page:" +str(i)
-             
-             cv.setLineWidth(5)
-             PAGE_HEIGHT = cv._pagesize[1]
-             cv.setStrokeColor(green)
-             cv.setFillColor(green)
-             pdf_text_object = cv.beginText(10,PAGE_HEIGHT-10) 
-             pdf_text_object.textOut(title)
-             
-             cv.drawText(pdf_text_object)
-             cv.save()
-             packet.seek(0)
-             tmp = PdfFileReader(packet)
-             template_page= tmp.getPage(0)
-             page = pdf_file.getPage(i)
-             page.cropBox.upperLeft = (10,500)
-             page.cropBox.lowerRight = (1200,1000)
+                i = random.randrange(i,num_pages)
+                packet = BytesIO()
+                cv=canvas.Canvas(packet,pagesize=letter)
 
-             total_width = max(page.mediaBox.upperRight[0],template_page.mediaBox.upperRight[0])
-             total_height = max([page.mediaBox.upperRight[1], template_page.mediaBox.upperRight[1]])
 
-             w = page.mediaBox.upperRight[0] - template_page.mediaBox.upperRight[0]
-             h = page.mediaBox.upperRight[1] - template_page.mediaBox.upperRight[1]
-             new_page = PageObject.createBlankPage(None, total_width, page.mediaBox.upperRight[1])
-             new_page.mergePage(page)
-             new_page.mergeTranslatedPage(template_page,w,h)
-             
-             
-             #new_pdf = PdfFileWriter()
-             #new_pdf.addPage(outpage)
-             output.addPage(new_page)
-             out_pdf_file = os.path.join(new_path,"new"+"_"+fn[:-3]+str(i)+".jpg")
-             outputStream = open("out.pdf",'wb')
-             #output.write(outputStream)
-             output.write(outputStream)
-             outputStream.close()
-		      
-             images = convert_from_path('out.pdf')
-             images[0].save(out_pdf_file,"JPEG")
-              
-          except Exception as e:
-             print("error: "+str(e))
-                 
+                title = fn[:-3]+" page:" +str(i)
+
+                cv.setLineWidth(5)
+                PAGE_HEIGHT = cv._pagesize[1]
+                cv.setStrokeColor(green)
+                cv.setFillColor(green)
+                pdf_text_object = cv.beginText(10,PAGE_HEIGHT-10)
+                pdf_text_object.textOut(title)
+
+                cv.drawText(pdf_text_object)
+                cv.save()
+                packet.seek(0)
+                tmp = PdfFileReader(packet)
+                template_page= tmp.getPage(0)
+                page = pdf_file.getPage(i)
+                page.cropBox.upperLeft = (10,500)
+                page.cropBox.lowerRight = (1200,1000)
+
+                total_width = max(page.mediaBox.upperRight[0],template_page.mediaBox.upperRight[0])
+                total_height = max([page.mediaBox.upperRight[1], template_page.mediaBox.upperRight[1]])
+
+                w = page.mediaBox.upperRight[0] - template_page.mediaBox.upperRight[0]
+                h = page.mediaBox.upperRight[1] - template_page.mediaBox.upperRight[1]
+                new_page = PageObject.createBlankPage(None, total_width, page.mediaBox.upperRight[1])
+                new_page.mergePage(page)
+                new_page.mergeTranslatedPage(template_page,w,h)
+
+
+                #new_pdf = PdfFileWriter()
+                #new_pdf.addPage(outpage)
+                output.addPage(new_page)
+                out_pdf_file = os.path.join(new_path,"new"+"_"+fn[:-3]+str(i)+".jpg")
+                outputStream = open("out.pdf",'wb')
+                #output.write(outputStream)
+                output.write(outputStream)
+                outputStream.close()
+
+                images = convert_from_path('out.pdf')
+                images[0].save(out_pdf_file,"JPEG")
+
+            except Exception as e:
+                print("error: "+str(e))
+    
+
+
 
 if __name__ == '__main__':
     main()
